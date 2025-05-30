1f767eec9829b3ea59a815bf1176f2d26aa3e43d
Chandana HR <77909453+ChandanaHR@users.noreply.github.com>
2025-05-30 16:11:26 +0530

    Create day2node.js

---
+++ day2node.js
@@ -0,0 +1,42 @@
+//create a files folder and inside that create one.txt and two.txt
+//Read is used to read contents from file in one.txt : Hello i am chandana HR
+const fs = require('fs');
+const path = require('path');
+
+fs.readFile("./files/one.txt",(err,data)=>{
+    if(err) throw err;
+    console.log(data.toString())
+})
+//Instead of data.toString() we can give 'utf-8'
+fs.readFile("./files/one.txt",'utf-8',(err,data)=>{
+    if(err) throw err;
+    console.log(data)
+})
+console.log("Hello...") //to show the execution of aysnc
+//exit on uncaught exception
+process.on('uncaughtException',err=>{
+    console.error(`There was an uncaught error: ${err}`);
+    process.exit(1);
+})
+
+fs.readFile(path.join(__dirname,'files','one.txt'),'utf-8',(err,data)=>{
+    if(err) throw err;
+    console.log(data)
+})
+
+//write operation is used to write some content inside a file by creating a file dynamically. initially only file wont be created
+//writefile
+fs.writeFile(path.join(__dirname,'files','write.txt'),'Nice to meet you',(err)=>{
+    if(err) throw err;
+    console.log("write operation complete")
+    fs.appendFile(path.join(__dirname,'files','write.txt'),'\n\nNice to meet you again',(err)=>{
+    if(err) throw err;
+    console.log("Append operation complete")
+    fs.rename(path.join(__dirname,'files','write.txt'),path.join(__dirname,'files','newwrite.txt'),(err)=>{
+    if(err) throw err;
+    console.log("Rename operation complete")
+})
+})
+})
+//Above method leads to callback hell
+
